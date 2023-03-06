f81b2e03f553c0eaf6c584f0a759bdad7f535516
Sarah Santana <sarah7bvs@gmail.com>
2023-03-06 12:38:27 +0000

    Metodo de busca por ID

--- src/main/java/br/gov/sp/fatec/springtopicos20231/service/UsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springtopicos20231/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.gov.sp.fatec.springtopicos20231.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,14 @@ public class UsuarioService {
 
     public List<Usuario> buscarTodosUsuarios() {
         return usuarioRepo.findAll();
+    }
+
+    public Usuario buscarPorId(Long id) {
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+        if(usuarioOp.isEmpty()) {
+            throw new IllegalArgumentException("Usuário não existe!");
+        }
+        return usuarioOp.get();
     }
     
 }
