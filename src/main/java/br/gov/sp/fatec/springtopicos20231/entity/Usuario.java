0ecfe603086b860a3c9726acf4a48d34656cbcbd
Sarah Santana <sarah7bvs@gmail.com>
2023-04-10 12:50:40 +0000

    Atualização do usuário

--- src/main/java/br/gov/sp/fatec/springtopicos20231/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/springtopicos20231/entity/Usuario.java
@@ -1,11 +1,19 @@
 package br.gov.sp.fatec.springtopicos20231.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "usr_usuario")
@@ -21,6 +29,17 @@ public class Usuario {
 
     @Column(name = "usr_senha")
     private String senha;
+
+    @ManyToMany
+    @JoinTable(name = "uau_usuario_autorizacao",
+        joinColumns = { @JoinColumn(name = "usr_id")},
+        inverseJoinColumns = { @JoinColumn(name = "aut_id") }
+        )
+    private List<Autorizacao> autorizacoes;
+
+    @OneToMany(mappedBy = "usuario")
+    @JsonIgnore
+    private List<Anotacao> anotacoes;
 
     public Usuario(String nome, String senha) {
         this.nome = nome;
@@ -53,6 +72,22 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public List<Autorizacao> getAutorizacoes() {
+        return autorizacoes;
+    }
+
+    public void setAutorizacoes(List<Autorizacao> autorizacoes) {
+        this.autorizacoes = autorizacoes;
+    }
+
+    public List<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(List<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
     }
     
 }
